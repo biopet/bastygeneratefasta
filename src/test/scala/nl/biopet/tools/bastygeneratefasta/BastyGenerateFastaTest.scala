07ce400e957dfe23ec4061a2a195cbc2e8a3d3d0
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/bastygeneratefasta/BastyGenerateFastaTest.scala
+++ src/test/scala/nl/biopet/tools/bastygeneratefasta/BastyGenerateFastaTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.bastygeneratefasta
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object BastyGenerateFastaTest extends BiopetTest {
+class BastyGenerateFastaTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
