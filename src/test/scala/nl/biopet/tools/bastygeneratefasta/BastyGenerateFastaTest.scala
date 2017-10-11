a0b007aec38b27ec618dd296956fe316972da5ef
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:34:34 +0200

    Fixed compile issue

--- src/test/scala/nl/biopet/tools/bastygeneratefasta/BastyGenerateFastaTest.scala
+++ src/test/scala/nl/biopet/tools/bastygeneratefasta/BastyGenerateFastaTest.scala
@@ -7,7 +7,7 @@ object BastyGenerateFastaTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      BastyGenerateFasta.main(Array())
     }
   }
 }
