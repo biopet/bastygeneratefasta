026b7aaedd54f6991f272dbce72f9c07aff2deac
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-12-28 11:42:10 +0100

    remove unbounded

--- src/main/scala/nl/biopet/tools/bastygeneratefasta/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/bastygeneratefasta/ArgsParser.scala
@@ -9,46 +9,46 @@ import scala.collection.mutable.ListBuffer
 
 class ArgsParser(toolCommand: ToolCommand[Args])
     extends AbstractOptParser[Args](toolCommand) {
-  opt[File]('V', "inputVcf") unbounded () valueName "<file>" action { (x, c) =>
+  opt[File]('V', "inputVcf") valueName "<file>" action { (x, c) =>
     c.copy(inputVcf = x)
   } text "vcf file, needed for outputVariants and outputConsensusVariants" validate {
     x =>
       if (x.exists) success else failure("File does not exist: " + x)
   }
-  opt[File]("bamFile") unbounded () valueName "<file>" action { (x, c) =>
+  opt[File]("bamFile") valueName "<file>" action { (x, c) =>
     c.copy(bamFile = x)
   } text "bam file, needed for outputConsensus and outputConsensusVariants" validate {
     x =>
       if (x.exists) success else failure("File does not exist: " + x)
   }
-  opt[File]("outputVariants") maxOccurs 1 unbounded () valueName "<file>" action {
+  opt[File]("outputVariants") maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(outputVariants = x)
   } text "fasta with only variants from vcf file"
-  opt[File]("outputConsensus") maxOccurs 1 unbounded () valueName "<file>" action {
+  opt[File]("outputConsensus") maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(outputConsensus = x)
   } text "Consensus fasta from bam, always reference bases else 'N'"
-  opt[File]("outputConsensusVariants") maxOccurs 1 unbounded () valueName "<file>" action {
+  opt[File]("outputConsensusVariants") maxOccurs 1 valueName "<file>" action {
     (x, c) =>
       c.copy(outputConsensusVariants = x)
   } text "Consensus fasta from bam with variants from vcf file, always reference bases else 'N'"
-  opt[Unit]("snpsOnly") unbounded () action { (_, c) =>
+  opt[Unit]("snpsOnly") action { (_, c) =>
     c.copy(snpsOnly = true)
   } text "Only use snps from vcf file"
-  opt[String]("sampleName") unbounded () action { (x, c) =>
+  opt[String]("sampleName") action { (x, c) =>
     c.copy(sampleName = x)
   } text "Sample name in vcf file"
-  opt[String]("outputName") required () unbounded () action { (x, c) =>
+  opt[String]("outputName") required () action { (x, c) =>
     c.copy(outputName = x)
   } text "Output name in fasta file header"
-  opt[Int]("minAD") unbounded () action { (x, c) =>
+  opt[Int]("minAD") action { (x, c) =>
     c.copy(minAD = x)
   } text "min AD value in vcf file for sample. Defaults to: 8"
-  opt[Int]("minDepth") unbounded () action { (x, c) =>
+  opt[Int]("minDepth") action { (x, c) =>
     c.copy(minDepth = x)
   } text "min depth in bam file. Defaults to: 8"
-  opt[File]("reference") unbounded () action { (x, c) =>
+  opt[File]("reference") action { (x, c) =>
     c.copy(reference = x)
   } text "Indexed reference fasta file" validate { x =>
     if (x.exists) success else failure("File does not exist: " + x)
