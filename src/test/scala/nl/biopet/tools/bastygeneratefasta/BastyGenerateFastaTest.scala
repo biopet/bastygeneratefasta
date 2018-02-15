9aea60583d497c6ed2dfd5aa1a0eba6d36a4f303
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/bastygeneratefasta/BastyGenerateFastaTest.scala
+++ src/test/scala/nl/biopet/tools/bastygeneratefasta/BastyGenerateFastaTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.bastygeneratefasta
 
 import java.io.File
@@ -8,7 +29,7 @@ import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import org.testng.annotations.Test
 
-class BastyGenerateFastaTest extends ToolTest[Args] with MockitoSugar{
+class BastyGenerateFastaTest extends ToolTest[Args] with MockitoSugar {
   def toolCommand: BastyGenerateFasta.type = BastyGenerateFasta
 
   import BastyGenerateFasta._
@@ -29,15 +50,15 @@ class BastyGenerateFastaTest extends ToolTest[Args] with MockitoSugar{
     tmp.deleteOnExit()
 
     val arguments = Array("-V",
-      chrQPath,
-      "--outputVariants",
-      tmppath,
-      "--sampleName",
-      "Sample_101",
-      "--reference",
-      chrQRefPath,
-      "--outputName",
-      "test")
+                          chrQPath,
+                          "--outputVariants",
+                          tmppath,
+                          "--sampleName",
+                          "Sample_101",
+                          "--reference",
+                          chrQRefPath,
+                          "--outputName",
+                          "test")
     main(arguments)
   }
 
@@ -48,17 +69,17 @@ class BastyGenerateFastaTest extends ToolTest[Args] with MockitoSugar{
     tmp.deleteOnExit()
 
     val arguments = Array("-V",
-      chrQPath,
-      "--outputVariants",
-      tmppath,
-      "--bamFile",
-      bamPath,
-      "--sampleName",
-      "Sample_101",
-      "--reference",
-      chrQRefPath,
-      "--outputName",
-      "test")
+                          chrQPath,
+                          "--outputVariants",
+                          tmppath,
+                          "--bamFile",
+                          bamPath,
+                          "--sampleName",
+                          "Sample_101",
+                          "--reference",
+                          chrQRefPath,
+                          "--outputName",
+                          "test")
     main(arguments)
   }
 
